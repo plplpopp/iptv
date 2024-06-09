f3bce76116dac72d0d828613485de4302a8d86b7
plplpopp <94629194+plplpopp@users.noreply.github.com>
2024-06-09 15:13:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,16 @@ response_time_weight = 0.5
 resolution_weight = 0.5
 open_sort = True
 # 1: 显示默认值，线路1 线路2，2：显示视频分辨率，如:1080x720
-xianlu_type = 2
+xianlu_type = 1
 # ffmpeg解析视频时间，单位秒
 ffmpeg_time = 10
 # key: 地区，在http://tonkiang.us网站上搜索的关键词
 # value: 订阅url，在https://github.com/xisohi/IPTV-Multicast-source中找自己想要的
 search_dict = {
     "上海": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/shanghai/telecom.txt"
+    "安徽": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/anhui/telecom.txt"
+    "河南": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/henan/telecom.txt"
+    "北京": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/xisohi/IPTV-Multicast-source/main/beijing/unicom.txt"
 }
 # 在http://tonkiang.us网站上搜索的源的页数
 search_page_num = 5
